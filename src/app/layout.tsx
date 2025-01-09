3da484f006729dc6bac21c2fb065a6485ea5ba13
couvq <quentincouvelaire@gmail.com>
2025-01-09 11:32:38 -0500

    added clerk auth, stupid easy setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,29 @@
-import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Clerk test application",
-  description: "Basic application to test clerk authentication library.",
-};
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
