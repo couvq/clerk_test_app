b157f437f9e734e3cbe5e08dcfad609571f74f33
couvq <quentincouvelaire@gmail.com>
2025-01-09 11:46:05 -0500

    basic example of reading session claims in a server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import { auth } from "@clerk/nextjs/server";
 
-const Home = () => {
-  return <>Unauthenticated landing page</>;
+const Home = async () => {
+  const { sessionClaims } = await auth();
+  const firstname = sessionClaims?.firstname;
+
+  return (
+    <>
+      {
+        firstname ? <h1><>Hello {firstname}</></h1> : null
+      }
+    </>
+  );
 };
 
 export default Home;
