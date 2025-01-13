cc844f740c8411ae08434e5fc67c0629d8b24438
couvq <quentincouvelaire@gmail.com>
2025-01-13 16:57:11 -0500

    example of generating and sending a token to a backend

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,19 +1,18 @@
-import { auth } from '@clerk/nextjs/server'
-import React from 'react'
+import { auth } from "@clerk/nextjs/server";
+import React from "react";
 
 const UserPage = async () => {
- const { userId } = await auth();
- const fetchOptions = {
-     headers: {
-         'x-clerk-user-id': userId
-     }
- }
- // @ts-ignore
- const res = await fetch('http://localhost:5000/user', fetchOptions)
- const userResponse = await res.text();
-  return (
-    <div>{userResponse}</div>
-  )
-}
+  const { getToken } = await auth();
+  const token = await getToken();
+  const fetchOptions = {
+    headers: {
+      Authorization: token,
+    },
+  };
+  // @ts-ignore
+  const res = await fetch("http://localhost:5000/user", fetchOptions);
+  const userResponse = await res.text();
+  return <div>{userResponse}</div>;
+};
 
-export default UserPage
+export default UserPage;
