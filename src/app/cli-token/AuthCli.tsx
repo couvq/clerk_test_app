7fc57624fe7a5168e8688a7b9d7f522a773b377d
couvq <quentincouvelaire@gmail.com>
2025-01-12 21:44:13 -0500

    adding loading indicator to cli button

--- src/app/cli-token/AuthCli.tsx
+++ src/app/cli-token/AuthCli.tsx
@@ -1,22 +1,33 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+
+const getTokenAndRedirect = async (
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  setLoading(true);
+  const res = await fetch("/api/cli-token");
+  const token = await res.json();
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+  const redirectUrlString = params.get("redirectUrl") ?? "";
+  const redirectUrl = new URL(redirectUrlString);
+  redirectUrl.searchParams.append("token", token);
+  setLoading(false);
+  window.open(redirectUrl);
+};
 
 export const AuthCli = () => {
-  const getTokenAndRedirect = async () => {
-    const res = await fetch("/api/cli-token");
-    const token = await res.json();
-    const url = new URL(window.location.href);
-    const params = new URLSearchParams(url.search);
-    const redirectUrlString = params.get("redirectUrl") ?? "";
-    const redirectUrl = new URL(redirectUrlString)
-    redirectUrl.searchParams.append('token', token)
-    window.open(redirectUrl)
-  };
-
+  const [loading, setLoading] = useState(false);
   return (
     <div>
       <h2>Do you want to authorize this CLI?</h2>
-      <button onClick={getTokenAndRedirect}>Yes, sign me in</button>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <button onClick={() => getTokenAndRedirect(setLoading)}>
+          Yes, sign me in
+        </button>
+      )}
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
 
 const Home = async () => {
-  const { userId, getToken } = await auth();
-  console.log(userId);
-  const token = await getToken();
-  console.log(token);
-
   return (
     <>
       <SignedOut>
